c41f1f9b907f261414b2fa398f64862322a92673
FedorKandiranda <s3kaaZa@gmail.com>
2024-06-27 13:31:25 +0300

    change arrow on accordion

--- js/accordion.js
+++ js/accordion.js
@@ -3,8 +3,12 @@ let i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
+        const panel = this.nextElementSibling;
+        const arrow = this.querySelector('.arrow');
+        
         this.classList.toggle("active");
-        var panel = this.nextElementSibling;
+        arrow.classList.toggle("active");
+
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
