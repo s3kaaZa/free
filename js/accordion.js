51f643d8b9cb80a915ae942f4564948641f407fe
FedorKandiranda <s3kaaZa@gmail.com>
2024-07-12 13:58:28 +0300

    remove weekend, change accordion color and border

--- js/accordion.js
+++ js/accordion.js
@@ -3,9 +3,11 @@ let i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
+        const parent = this.parentElement;
         const panel = this.nextElementSibling;
         const arrow = this.querySelector('.arrow');
         
+        parent.classList.toggle("active");
         this.classList.toggle("active");
         arrow.classList.toggle("active");
 
